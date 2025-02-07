5881939fde3c5d0d85a01077c5a6aabe9c651907
Ji Liu <liuji1031@gmail.com>
2025-02-07 10:34:25 -0500

    fix: move activation dict within class

--- model/mlp.py
+++ model/mlp.py
@@ -1,9 +1,10 @@
 import typing
+
 import torch
 from torch.nn import Linear
 
 from model.registry import Module
-from config.activations import activation_dict
+
 
 class Mlp(Module):
     """multi-layer perceptron
@@ -11,24 +12,33 @@ class Mlp(Module):
     Args:
         torch (_type_): _description_
     """
-    def __init__(self,
-                 inp_dim : int,
-                 hidden_dim_lst : typing.List[int],
-                 out_dim : int,
-                 activation : str = "relu",
-                 output_activation : str = "none",
-                 add_batch_norm : bool = False,
-                 return_dict : bool = False,
-                 **kwargs):
+
+    ACTIVATION_DICT = {
+        "relu": torch.nn.ReLU,
+        "tanh": torch.nn.Tanh,
+        "none": torch.nn.Identity,
+    }
+
+    def __init__(
+        self,
+        inp_dim: int,
+        hidden_dim_lst: typing.List[int],
+        out_dim: int,
+        activation: str = "relu",
+        output_activation: str = "none",
+        add_batch_norm: bool = False,
+        return_dict: bool = False,
+        **kwargs,
+    ):
         """_summary_
 
         Args:
             dim_lst (typing.List[int]): list of input and hidden layer dims
             the output dimension is always 3
-            activation (str, optional): activation function of intermediate 
+            activation (str, optional): activation function of intermediate
             layers. defaults to "relu".
             output_activation (str, optional): activation function of the output
-            layer. defaults to "none". 
+            layer. defaults to "none".
             add_batch_norm (bool, optional): whether to add batch normalization
             return_dict (bool, optional): whether to return the output as a dict
         """
@@ -41,16 +51,16 @@ class Mlp(Module):
             layers.append(Linear(in_dims[i], out_dims[i]))
             if i < n_layers - 1:
                 if activation != "none":
-                    layers.append(activation_dict[activation]())
-            else: # last layer
+                    layers.append(self.ACTIVATION_DICT[activation]())
+            else:  # last layer
                 if output_activation != "none":
-                    layers.append(activation_dict[output_activation]())
-            
+                    layers.append(self.ACTIVATION_DICT[output_activation]())
+
             if add_batch_norm:
                 layers.append(torch.nn.BatchNorm1d(out_dims[i]))
 
         self.mlp = torch.nn.Sequential(*layers)
-    
+
     def forward(self, x):
         if self.return_dict:
             return {"x": self.mlp(x)}
