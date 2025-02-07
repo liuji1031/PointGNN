fix: correct regression target computation

dataset/dataset.py
@@ -1,22 +1,22 @@
+import multiprocessing as mp
 import os
+import pathlib
 from typing import List, Tuple, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pathlib
-import multiprocessing as mp
 import torch
-from tqdm import tqdm
-from torch_geometric.data import Data, Dataset
-from torch_geometric.transforms import Compose
 from nuscenes.nuscenes import NuScenes
 from nuscenes.utils.data_classes import Box, LidarPointCloud
 from nuscenes.utils.geometry_utils import transform_matrix
 from pyquaternion import Quaternion
+from torch_geometric.data import Data, Dataset
+from torch_geometric.transforms import Compose
+from tqdm import tqdm
 
 from bbox import BoundingBox3D
 from dataset.augment import AugmentRegistry
-from dataset.preprocess import PreprocessRegistry, PreprocessPointCloud
+from dataset.preprocess import PreprocessPointCloud, PreprocessRegistry
 from dataset.util import encode_loc_reg_target
 
 
@@ -96,11 +96,12 @@ class NuScenesDataset(Dataset):
         super().__init__(
             root=data_root, transform=None, pre_transform=None, pre_filter=None
         )
+
     @property
     def processed_dir(self):
         """Return the processed directory."""
         return str(pathlib.Path(self.data_root) / f"processed_{self.mode}")
-    
+
     @property
     def num_classes(self):
         """Return the number of classes in the dataset."""
@@ -132,7 +133,7 @@ class NuScenesDataset(Dataset):
         """
         # figure out the number of digits in the length of the dataset
         n_digits = len(str(self.len()))
-        return [f"data_{i+1:0{n_digits}d}.pt" for i in range(self.len())]
+        return [f"data_{i + 1:0{n_digits}d}.pt" for i in range(self.len())]
 
     def download(self):
         """Download the dataset.
@@ -152,7 +153,7 @@ class NuScenesDataset(Dataset):
 
             # apply preprocessings
             for pre in self.preprocesses:
-                pre : PreprocessPointCloud
+                pre: PreprocessPointCloud
                 pre.preprocess(points)
 
             # create torch geometric data from points
@@ -167,7 +168,7 @@ class NuScenesDataset(Dataset):
             torch.save(data, processed_dir / fn)
 
         for i, (sample_token, fn) in enumerate(
-            tqdm(zip(self.sample_tokens, self.processed_file_names),total=self.len())
+            tqdm(zip(self.sample_tokens, self.processed_file_names), total=self.len())
         ):
             _process(sample_token, fn)
 
@@ -187,8 +188,17 @@ class NuScenesDataset(Dataset):
         # do augmentations on the data
         if self.augmentations is not None:
             data = self.augmentations(data)
-        return data
 
+        # compute localization regression target
+        reg_xyz,reg_lwh,reg_r,positive_mask,obj_cls_label = self._compute_loc_regression_target(
+            data.gt_boxes, data.pos
+        )
+        data.reg_xyz = reg_xyz
+        data.reg_lwh = reg_lwh
+        data.reg_r = reg_r
+        data.positive_mask = positive_mask
+        data.obj_cls_label = obj_cls_label
+        return data
 
     def _parse_augment_config(self, augment_config: list):
         """Parse the augmentation configuration.
@@ -499,7 +509,7 @@ class NuScenesDataset(Dataset):
         return pc, sensor_loc
 
     def _compute_loc_regression_target(
-        self, gt_boxes: List[BoundingBox3D], points: LidarPointCloud
+        self, gt_boxes: List[BoundingBox3D], pos: torch.Tensor
     ):
         """Compute the regression target for localization head.
 
@@ -509,26 +519,28 @@ class NuScenesDataset(Dataset):
         Returns:
             np.ndarray: 1D array of labels
         """
-        n = points.nbr_points()
-        loc_regression_target = np.zeros((n, 7), dtype=np.float32)
-        positive_mask = np.zeros(n, dtype=bool)
+        n = pos.shape[0]
+        positive_mask = torch.from_numpy(np.zeros(n, dtype=bool))
+        box_xyz = torch.from_numpy(np.zeros((n, 3), dtype=np.float32))
+        box_lwh = torch.from_numpy(np.zeros((n, 3), dtype=np.float32))
+        box_r = torch.from_numpy(np.zeros((n, 1), dtype=np.float32))
+        positive_mask = torch.from_numpy(np.zeros(n, dtype=bool))
+        obj_cls_label = torch.from_numpy(np.zeros(n, dtype=np.int64))
         for gt_box in gt_boxes:
-            mask = gt_box.within_gt_box(points.points[:3, :].T)
-
-            print(np.sum(mask))
-
-            # compute localization regression target for each point within the gt box
-            encode_loc_reg_target(
-                reg_target=loc_regression_target,
-                box_xyz=gt_box.xyz_np,
-                box_lhw=gt_box.lwh_np,
-                box_r=gt_box.r_np,
-                ref_box=self.avg_anchor_box,
-                points=points,
-                mask=mask,
-            )
-
-            # add to the positive mask
+            mask = gt_box.within_gt_box(pos.numpy())
+            box_xyz[mask, :] = gt_box.xyz_tensor.to(torch.float32)
+            box_lwh[mask, :] = gt_box.lwh_tensor.to(torch.float32)
+            box_r[mask, :] = gt_box.r_tensor.to(torch.float32)
             positive_mask[mask] = True
+            obj_cls_label[mask] = gt_box.label
 
-        return loc_regression_target, positive_mask
+        # compute localization regression target for each point within the gt box
+        reg_xyz, reg_lwh, reg_r = encode_loc_reg_target(
+            box_xyz=box_xyz,
+            box_lwh=box_lwh,
+            box_r=box_r,
+            ref_box_lwh=self.avg_anchor_box.lwh_tensor,
+            pos=pos,
+        )
+
+        return reg_xyz, reg_lwh, reg_r, positive_mask, obj_cls_label

dataset/util.py
@@ -1,60 +1,50 @@
-import numpy as np
 from typing import Union
+
+import numpy as np
 import torch
 from einops import rearrange
-from bbox import BoundingBox3D
 from nuscenes.utils.data_classes import LidarPointCloud
 
-def encode_loc_reg_target(reg_target:Union[np.ndarray,torch.Tensor],
-                          box_xyz:Union[np.ndarray,torch.Tensor],
-                          box_lhw:Union[np.ndarray,torch.Tensor],
-                          box_r:Union[np.ndarray,torch.Tensor],
-                          ref_box:BoundingBox3D,
-                          points:LidarPointCloud,
-                          mask:np.ndarray):
+from bbox import BoundingBox3D
+
+
+def encode_loc_reg_target(
+    box_xyz: Union[np.ndarray, torch.Tensor],
+    box_lwh: Union[np.ndarray, torch.Tensor],
+    box_r: Union[np.ndarray, torch.Tensor],
+    ref_box_lwh: Union[np.ndarray, torch.Tensor],
+    pos: Union[np.ndarray, torch.Tensor],
+):
     """Encode the regression target for localization.
 
-    Given a point cloud, and a mask that indicates the points within the ground 
+    Given a point cloud, and a mask that indicates the points within the ground
     truth box, encode the regression target and stores them in reg_target
 
     Args:
         reg_target (np.ndarray): 2D array of regression targets
         box_xyz (np.ndarray): target box xyz
-        box_lhw (np.ndarray): target box lhw
+        box_lwh (np.ndarray): target box lhw
         box_r (np.ndarray): target box rotation
         ref_box (BoundingBox3D): reference box
         points (LidarPointCloud): point cloud
         mask (np.ndarray): mask that indicates the points within the ground truth box
     """
+    assert pos.shape[-1] == 3, "Position must have 3 dimensions"
     # x, y, z target
-    if box_xyz.ndim == 1:
-        box_xyz = rearrange(box_xyz,'d -> 1 d')
     if isinstance(box_xyz, torch.Tensor):
-        ref_box_lhw = ref_box.lwh_tensor
-        log = torch.log
-        points_xyz = torch.from_numpy(points.points[:3,:].T)
+        log, atan2, sin, cos, pi = torch.log, torch.atan2, torch.sin, torch.cos, torch.pi
     else:
-        ref_box_lhw = ref_box.lwh_np
-        log = np.log
-        points_xyz = points.points[:3,:].T
-    reg_target[mask,:3] = ((box_xyz - points_xyz)/ref_box_lhw)[mask,:]
+        log,atan2, sin, cos,pi = np.log,np.arctan2, np.sin, np.cos, np.pi
+    reg_xyz = (box_xyz - pos) / ref_box_lwh
+    reg_lwh = log(box_lwh / ref_box_lwh)
+    reg_r = atan2(sin(box_r), cos(box_r)) / pi
 
-    # l, w, h target
-    if box_lhw.ndim == 1:
-        box_lhw = rearrange(box_lhw,'d -> 1 d')
-    reg_target[mask,3:6] = log(box_lhw/ref_box_lhw)
-
-    # rotation target, scaled to [-1,1]
-    if box_r.ndim == 1:
-        box_r = rearrange(box_r,'d -> 1 d')
-    if isinstance(box_r, torch.Tensor):
-        atan2, sin, cos, pi = torch.atan2, torch.sin, torch.cos,torch.pi
-    else:
-        atan2, sin, cos, pi = np.arctan2, np.sin, np.cos, np.pi
-    reg_target[mask,[6]] = atan2(sin(box_r), cos(box_r))/pi
+    return reg_xyz, reg_lwh, reg_r
 
 
-def decode_bbox(loc_output:torch.Tensor, ref_box:BoundingBox3D, points:LidarPointCloud):
+def decode_bbox(
+    loc_output: torch.Tensor, ref_box: BoundingBox3D, points: LidarPointCloud
+):
     """Decode the bounding box from the output of the network.
 
     Args:
@@ -67,16 +57,16 @@ def decode_bbox(loc_output:torch.Tensor, ref_box:BoundingBox3D, points:LidarPoin
         Tensor : 2D tensor of bounding boxes
     """
     n = loc_output.shape[0]
-    bbox = torch.zeros((n,7),dtype=torch.float32)
-    point_xyz = torch.from_numpy(points.points[:3,:].T)
+    bbox = torch.zeros((n, 7), dtype=torch.float32)
+    point_xyz = torch.from_numpy(points.points[:3, :].T)
 
     # xyz
-    bbox[:,:3] = loc_output[:,:3]*ref_box.tensor[[0],:3] + point_xyz
+    bbox[:, :3] = loc_output[:, :3] * ref_box.tensor[[0], :3] + point_xyz
 
     # lwh
-    bbox[:,3:6] = torch.exp(loc_output[:,3:6])*ref_box.tensor[[0],3:6]
+    bbox[:, 3:6] = torch.exp(loc_output[:, 3:6]) * ref_box.tensor[[0], 3:6]
 
     # rotation
-    bbox[:,6] = loc_output[:,6]*torch.pi
+    bbox[:, 6] = loc_output[:, 6] * torch.pi
 
     return bbox