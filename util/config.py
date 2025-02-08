27298b0fb110ff8d07775071a3eb0f81c8d6c3e3
Ji Liu <liuji1031@gmail.com>
2025-02-07 21:55:14 -0500

    feat: return config as edict

--- util/config.py
+++ util/config.py
@@ -1,7 +1,9 @@
 import re
 import yaml
+import pathlib
+from easydict import EasyDict as edict
 
-def read_config(path : str) -> dict:
+def read_config(path: str | pathlib.Path) -> edict:
     """read a yaml file and return a dictionary
 
     Args:
@@ -10,9 +12,10 @@ def read_config(path : str) -> dict:
     Returns:
         dict: dictionary of the yaml file
     """
-    with open(path, 'r') as file:
-        return yaml.safe_load(file)
-    
+    with open(path, "r") as file:
+        return edict(yaml.safe_load(file))
+
+
 def camel_to_snake(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
