ea62d2c2b82f1de6c9538be0bf39012a5e87d9c1
Ji Liu <liuji1031@gmail.com>
2025-02-12 14:43:55 -0500

    feat: add build fcn from config for model and dataset

--- util/__init__.py
+++ util/__init__.py
@@ -1,9 +1,11 @@
-from util.config import camel_to_snake, read_config
+from util.config import camel_to_snake, read_config, build_model, build_dataset
 from util.plot import plot_torch_geo_data, to_lidar_point_cloud
 
 __all__ = [
     "read_config",
+    "build_model",
     "camel_to_snake",
     "plot_torch_geo_data",
     "to_lidar_point_cloud",
+    "build_dataset",
 ]
--- util/config.py
+++ util/config.py
@@ -2,6 +2,9 @@ import re
 import yaml
 import pathlib
 from easydict import EasyDict as edict
+from model import ComposableModel
+from dataset import DatasetRegistry
+
 
 def read_config(path: str | pathlib.Path) -> edict:
     """read a yaml file and return a dictionary
@@ -19,3 +22,29 @@ def read_config(path: str | pathlib.Path) -> edict:
 def camel_to_snake(name):
     s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+
+
+def build_model(config_file: str):
+    """Build full model based on config file.
+
+    Args:
+        config_file (dic
+    Returns:
+        torch.nn.Module: the model
+    """
+    config = read_config(config_file)
+    assert "model" in config, "Model configuration not found in the config file"
+    model = ComposableModel(
+        config.model.get("name", "model"), config.model.modules
+    )
+
+    return model
+
+
+def build_dataset(config_file: str, mode="train"):
+    data_cfg = read_config(config_file)
+    return DatasetRegistry.build(
+        mode=mode,
+        name=data_cfg.name,
+        config=data_cfg.config,
+    )
